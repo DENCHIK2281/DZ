ce9e35cee8e05e00014c836e6e6e029c51347c60
DENCHIK2281 <163615246+DENCHIK2281@users.noreply.github.com>
2024-04-11 13:17:43 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main() 
+    {
+        string str = "абобка";
+        char[] arr = str.ToCharArray();
+        arr[0] = ' ';
+        arr[arr.Length - 1] = '.';
+        str = new string(arr);
+        Console.WriteLine(str);
+    }
+ 
+    
+        
+        
+}
