7ae309274a0b4918ba0cf5da51bc7357b6e84a68
DENCHIK2281 <163615246+DENCHIK2281@users.noreply.github.com>
2024-04-11 13:18:21 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] array = new char[10];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+            int замены = 0;
+
+            Console.WriteLine("----");
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] == ':')
+                {
+                    array[i] = ';';
+                    замены++;
+                }
+                Console.WriteLine(array[i]);
+
+            }
+            Console.WriteLine("Замены: " + замены);
+            Console.ReadKey();
+        }
+        
+    }
+}
